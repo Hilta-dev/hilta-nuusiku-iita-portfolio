bf6dc5aefdb596bb37774afc6f2ad69f9ddb1463
Hilta <hiltan004@gmail.com>
2026-09-22 15:46:31 +0200

    Fix Vercel portfolio deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hilta-nuusiku-iita-portfolio/',
+  base: process.env.GITHUB_ACTIONS
+    ? '/hilta-nuusiku-iita-portfolio/'
+    : '/',
 })
